4495be855e86da9b6ed84df0916c9b72e148395a
Kirill <kirill.rylskij96@gmail.com>
2026-06-19 14:55:03 +0300

    выполнение дз 30.1

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'rest_framework',
+    'users',
+    'materials',
 ]
 
 MIDDLEWARE = [
@@ -83,6 +85,9 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
+AUTH_USER_MODEL = 'users.User'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 AUTH_PASSWORD_VALIDATORS = [
     {
